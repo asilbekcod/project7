8d99ddf20d2f2e71361fc032673d0c3443de90d2
asilbekcod <isarvakoriya@gmail.com>
2023-02-22 11:36:17 +0500

    master

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
