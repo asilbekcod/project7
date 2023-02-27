fd2911bd515a97a3f161c47e6b68f304c6c0bfc7
azizbekcodemy <azizbekobidiov0303@gmail.com>
2023-02-27 12:55:35 +0300

    navbar footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import Router from "./routes/routes";
+import ResponsiveAppBar from "./companents/Navbar";
+import Footer from "./companents/Futer";
+import "./index.css"
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <ResponsiveAppBar />
+        <Router />
+        <Footer />
       </BrowserRouter>
     </div>
   );
